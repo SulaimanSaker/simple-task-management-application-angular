ee26eab377bbcb0f0a69cc70412b74976ca9fd84
Sulaiman Saker <sulaiman.saker@hotmail.com>
2023-12-22 10:54:56 +0400

    Add form validation to the login and register forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +24,7 @@ import { LoginComponent } from './components/login/login.component';
     FormsModule,
     HttpClientModule,
     FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import CreateUserDto from 'src/app/dtos/create-user.dto';
 import LoginUserDto from 'src/app/dtos/login-user.dto';
 import { AuthService } from 'src/app/services/auth.service';
@@ -12,58 +13,82 @@ export class LoginComponent implements OnInit {
   @Output() loginStatus: EventEmitter<boolean> = new EventEmitter();
 
   title: string = 'Todo List';
-
   isLogin: boolean = true;
 
-  loginEmail: string = '';
-  loginPassword: string = '';
+  loginForm: FormGroup;
+  registerForm: FormGroup;
 
-  registerEmail: string = '';
-  registerName: string = '';
-  registerPassword: string = '';
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      loginEmail: ['', [Validators.required, Validators.email]],
+      loginPassword: ['', Validators.required],
+    });
 
-  constructor(private authService: AuthService) {}
+    this.registerForm = this.fb.group({
+      registerName: ['', Validators.required],
+      registerEmail: ['', [Validators.required, Validators.email]],
+      registerPassword: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
-    this.loginEmail = '';
-    this.loginPassword = '';
-
-    this.registerEmail = '';
-    this.registerName = '';
-    this.registerPassword = '';
+    this.loginForm.reset();
+    this.registerForm.reset();
   }
 
   onLoginSubmit() {
-    const loginUserDto: LoginUserDto = {
-      email: this.loginEmail,
-      password: this.loginPassword,
-    };
+    if (this.loginForm.valid) {
+      const loginUserDto: LoginUserDto = {
+        email: this.loginForm.value.loginEmail,
+        password: this.loginForm.value.loginPassword,
+      };
 
-    this.authService.loginUser(loginUserDto).subscribe((res) => {
-      this.ngOnInit();
-
-      localStorage.setItem('user', JSON.stringify(res.data.user));
-
-      localStorage.setItem('token', res.data.token);
-
-      this.sendLoginStatus();
-    });
+      this.authService.loginUser(loginUserDto).subscribe((res) => {
+        this.ngOnInit();
+        localStorage.setItem('user', JSON.stringify(res.data.user));
+        localStorage.setItem('token', res.data.token);
+        this.sendLoginStatus();
+      });
+    }
   }
 
   onRegisterSubmit() {
-    const createUserDto: CreateUserDto = {
-      name: this.registerName,
-      email: this.registerEmail,
-      password: this.registerPassword,
-    };
+    if (this.registerForm.valid) {
+      const createUserDto: CreateUserDto = {
+        name: this.registerForm.value.registerName,
+        email: this.registerForm.value.registerEmail,
+        password: this.registerForm.value.registerPassword,
+      };
 
-    this.authService.registerUser(createUserDto).subscribe(() => {
-      this.isLogin = true;
-      this.ngOnInit();
-    });
+      this.authService.registerUser(createUserDto).subscribe(() => {
+        this.isLogin = true;
+        this.ngOnInit();
+      });
+    }
   }
 
   sendLoginStatus() {
     this.loginStatus.emit(true);
+  }
+
+  // Getter methods for form controls
+  get loginEmail() {
+    return this.loginForm.get('loginEmail');
+  }
+
+  get loginPassword() {
+    return this.loginForm.get('loginPassword');
+  }
+
+  get registerName() {
+    return this.registerForm.get('registerName');
+  }
+
+  get registerEmail() {
+    return this.registerForm.get('registerEmail');
+  }
+
+  get registerPassword() {
+    return this.registerForm.get('registerPassword');
   }
 }
